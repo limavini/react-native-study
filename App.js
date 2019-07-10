01708db5b307328ba62c9fb1216136388caaf61c
stephen grider <ste.grider@gmail.com>
2019-07-10 12:21:44 -0600

    :rocket: Initial commit

--- App.js
+++ App.js
@@ -6,5 +6,20 @@ const navigator = createStackNavigator({
   Home: HomeScreen,
   Layout: LayoutScreen
 });
+import ComponentsScreen from "./src/screens/ComponentsScreen";
+
+const navigator = createStackNavigator(
+  // Rotas (caminho: componente)
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen
+  },
+  {
+    initialRouteName: 'Components',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
+  }
+);
 
 export default createAppContainer(navigator);
