f0247cfa4742da8a19b6c6629043fa6b1c8aa6d0
limavini <viniciusalmeidait@gmail.com>
2019-11-27 21:47:55 -0300

    Adding images pages

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = ({navigation}) => {
-
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hello World!</Text>
-      <Button onPress={() => navigation.navigate("Components")} title="Go to Components Demo"/>
-      <Button  onPress={() => navigation.navigate("List")} title="Go to List Demo">
-      </Button>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to List Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Image")}
+        title="Go to Image Demo"
+      />
     </View>
   );
 };
---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import ImageDetail from "../components/ImageDetail";
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <ImageDetail title="Forest" score={6} imageSource={require("../../assets/forest.jpg")}/>
+      <ImageDetail title="Beach" score={9} imageSource={require("../../assets/beach.jpg")}/>
+      <ImageDetail title="Mountain" score={10} imageSource={require("../../assets/mountain.jpg")}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;
